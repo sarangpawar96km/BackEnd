33450184774129007deb5238c61e8c76d525dac4
Sarang Pawar <pawarsarangnavnath.96km@gmail.com>
2024-11-09 08:28:28 +0530

    utils code added

--- src/index.js
+++ src/index.js
@@ -7,6 +7,18 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>{
+    app.on("Error", (error)=>{
+        console.log("ERRR: ",error);
+        throw error
+    })
+    app.listen(process.env.PORT || 3000, ()=>{
+        console.log(`Server is running at port: ${process.env.PORT || 3000}`);
+    })
+})
+.catch((err)=>{
+    console.log("MongoDB connection failed!!!", err);
+})
 
 
 
